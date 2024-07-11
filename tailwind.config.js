35c1c3e0b684d418a0541824f48f0f18667c9f2c
neerajhariyale <hariyaleneeraj31@gmail.com>
2024-07-11 15:28:54 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    letterSpacing: {
+      '1': '0em',
+      '2': '0.025em',
+      '3': '0.05em',
+      '4': '0.1em',
+      '5': '0.2em'
+
+    }
+  },
+  plugins: [],
+}
